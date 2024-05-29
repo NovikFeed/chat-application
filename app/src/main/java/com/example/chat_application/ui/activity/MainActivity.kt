068dcc9928b881935f76c7ae81d17e959e6a0677
ivanovich <vania200345@gmail.com>
2024-05-29 09:40:30 +0300

    Create navigation system between fragments and Add Loading, Login, Register fragments

--- app/src/main/java/com/example/chat_application/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/chat_application/ui/activity/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.example.chat_application.ui.activity.ui.theme.Chat_applicationTheme
 import com.example.core.NavigationViewModel
+import com.example.data.NavigationScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +39,12 @@ fun MyApp(modifier: Modifier = Modifier) {
 }
 @Composable
 fun MainContent(viewModel: NavigationViewModel){
-    val currentScreen by viewModel.currentScreen.observeAsState().value
+    val currentScreen by viewModel.currentScreen.observeAsState(NavigationScreen.Loading)
+    when(currentScreen){
+        NavigationScreen.Loading -> LoadingFragment()
+        NavigationScreen.Login -> LoginFragment()
+        NavigationScreen.Register -> RegisterFragment()
+
+    }
 }
 
