53cd13c63a4c83c06e80cabd9aeba7e14ba7a42b
ivanovich <vania200345@gmail.com>
2024-06-07 11:03:37 +0300

    Fill LoginFragment and Add login function

--- View/src/main/java/com/example/chat_application/ui/activity/LoginFragment.kt
+++ View/src/main/java/com/example/chat_application/ui/activity/LoginFragment.kt
@@ -3,15 +3,19 @@ package com.example.chat_application.ui.activity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,16 +27,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.chat_application.ui.activity.ui.theme.Images
 import com.example.core.LoginViewModel
-import com.example.core.NavigationViewModel
+import com.example.core.NavigationManager
 import com.example.core.NavigationScreen
-
 @Composable
-fun LoginFragment(viewModel : NavigationViewModel, viewModelText: LoginViewModel) {
+fun LoginFragment(viewModelText: LoginViewModel) {
     val styleInput = OutlinedTextFieldDefaults.colors(
         focusedBorderColor = MaterialTheme.colorScheme.onSurface,
         unfocusedBorderColor = MaterialTheme.colorScheme.onSurface,
@@ -42,7 +47,9 @@ fun LoginFragment(viewModel : NavigationViewModel, viewModelText: LoginViewModel
     val textEmail = remember { mutableStateOf("") }
     val textPassword = remember { mutableStateOf("") }
 
-    val visibility by viewModelText.emptyInput.observeAsState(false)
+    val visibility by viewModelText.emptyInput.observeAsState(initial = false)
+    val errorText by viewModelText.errorMassage.observeAsState(initial = "fill in all fields")
+
      Column(modifier = Modifier
          .fillMaxSize(1f)
          .padding(0.dp, 16.dp), horizontalAlignment = Alignment.CenterHorizontally,
@@ -51,31 +58,40 @@ fun LoginFragment(viewModel : NavigationViewModel, viewModelText: LoginViewModel
          Text(text = "Login to your account", style = MaterialTheme.typography.titleLarge)
          OutlinedTextField(value = textEmail.value, onValueChange = { viewModelText.setEmail(it)
                                                                      textEmail.value = it},
-             modifier = Modifier.padding(12.dp),
+             modifier = Modifier.fillMaxWidth().height(100.dp).padding(12.dp),
              label = { Text(text = "Email", fontSize = 25.sp)},
              colors = styleInput)
          OutlinedTextField(value = textPassword.value, onValueChange = { viewModelText.setPassword(it)
              textPassword.value = it},
-             modifier = Modifier.padding(12.dp),
+             modifier = Modifier.fillMaxWidth().height(100.dp).padding(12.dp),
              label = { Text(text = "Password", fontSize = 25.sp)},
              colors = styleInput,
              visualTransformation = PasswordVisualTransformation())
-         Button(modifier = Modifier
-             .fillMaxWidth(0.82f)
-             .fillMaxHeight(0.28f)
-             .padding(16.dp, 16.dp, 16.dp, 8.dp)
-             .background(MaterialTheme.colorScheme.onSurface, shape = RectangleShape),
-             onClick = {viewModelText.setDataForLogin()}) {
-             Text(text = "Log in",style = MaterialTheme.typography.bodyLarge, color = if (MaterialTheme.colorScheme.onSurface
-                 .luminance() > 0.5) Color.Black else Color.White,)
+         Surface {
+             Button(modifier = Modifier
+                 .fillMaxWidth(0.82f)
+                 .fillMaxHeight(0.28f)
+                 .padding(16.dp, 16.dp, 16.dp, 8.dp)
+                 .background(MaterialTheme.colorScheme.onSurface, shape = RectangleShape),
+                 onClick = { viewModelText.setDataForLogin() }) {
+                 Text(
+                     text = "Log in", style = MaterialTheme.typography.bodyLarge,
+                     color = if (MaterialTheme.colorScheme.onSurface
+                             .luminance() > 0.5
+                     ) Color.Black else Color.White,
+                 )
+             }
          }
-         Button(onClick = {viewModel.navigateTo(NavigationScreen.Register)}, Modifier.background(Color.Transparent)) {
+         Button(onClick = { NavigationManager.navigateTo(NavigationScreen.Register)}, Modifier.background(Color.Transparent)) {
              Text(text = "create new account",
                  style = MaterialTheme.typography.bodyMedium,
                  color = (MaterialTheme.colorScheme.onSurface))
          }
          if(visibility){
-             Text(text = "fill in all fields", color = Color.Red)
+
+             Text(text = errorText, color = Color.Red, modifier = Modifier
+                 .padding(16.dp, 0.dp)
+                 .wrapContentSize(Alignment.Center))
          }
 
 
--- ViewModel/src/main/java/com/example/core/LoginViewModel.kt
+++ ViewModel/src/main/java/com/example/core/LoginViewModel.kt
@@ -1,20 +1,43 @@
 package com.example.core
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.data.AuthRepository
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
-   private val _emptyInput = MutableLiveData<Boolean>()
-    val emptyInput = _emptyInput
-    val email= mutableStateOf("")
-    val password = mutableStateOf("")
+    private val _emptyInput = MutableLiveData<Boolean>()
+            val emptyInput = _emptyInput
+
+    private val _errorMassage = MutableLiveData<String>()
+            val errorMassage = _errorMassage
+
+    private val email= mutableStateOf("")
+    private val password = mutableStateOf("")
+
+    private val authRepository = AuthRepository()
 
     fun setDataForLogin(){
         if(email.value != "" && password.value != ""){
+            viewModelScope.launch{
+               val result = authRepository.loginToAccount(email.value, password.value)
+                Log.i("coord", result.toString())
+                if(result.isSuccess){
+                    NavigationManager.navigateTo(NavigationScreen.Chats)
+                }
+                else{
+                    _errorMassage.postValue(result.toString().substringAfter(":"))
+                    _emptyInput.postValue(true)
 
+
+                }
+            }
         }
         else{
+            _errorMassage.value = "fill in all fields"
             emptyInput.value = true
         }
     }
