80b5725ccfb7f5d72849ba1668010e652bfa44dd
ivanovich <vania200345@gmail.com>
2024-06-11 09:36:24 +0300

    Add register function and fill the register fragment with content

--- Model/src/main/java/com/example/data/AuthRepository.kt
+++ Model/src/main/java/com/example/data/AuthRepository.kt
@@ -1,23 +1,23 @@
 package com.example.data
 
+import android.net.Uri
+import android.provider.ContactsContract.CommonDataKinds.Nickname
 import android.util.Log
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.database
+import com.google.firebase.storage.storage
 import kotlinx.coroutines.tasks.await
+import java.io.File
 import java.lang.Exception
 
 class AuthRepository {
+     private lateinit var auth : FirebaseAuth
      suspend fun loginToAccount(email:String, password: String): Result<String>{
-          lateinit var auth : FirebaseAuth
-          try {
-               auth = Firebase.auth
-          }
-          catch (e : Exception){
-               Log.i("coord", e.toString())
-          }
           return try{
+               auth = Firebase.auth
                auth.signInWithEmailAndPassword(email, password).await()
                Result.success("Login successful")
           }
@@ -25,4 +25,23 @@ class AuthRepository {
                Result.failure(e)
           }
      }
+     suspend fun registerAccount(email: String, password: String, nickname: String, imgUri : Uri): Result<String>{
+          auth = Firebase.auth
+          val db = Firebase.database("https://chat-ab91b-default-rtdb.firebaseio.com/").reference.child("users")
+          val dbStorage = Firebase.storage("gs://chat-ab91b.appspot.com").reference
+          return try{
+               auth.createUserWithEmailAndPassword(email, password).await()
+               val currentUserUID = auth.currentUser?.uid
+               val imgInStorage = dbStorage.child("image:$currentUserUID")
+               imgInStorage.putFile(imgUri).await()
+               val urlInStorage = imgInStorage.downloadUrl.await()
+               db.child(currentUserUID!!).setValue(User(nickname, email, urlInStorage.toString())).await()
+               Result.success("Register successful")
+
+          }
+          catch(e : Exception){
+               Result.failure(e)
+          }
+     }
+
 }
--- ViewModel/src/main/java/com/example/core/AuthViewModel.kt
+++ ViewModel/src/main/java/com/example/core/AuthViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.core
 
+import android.net.Uri
+import android.provider.ContactsContract.CommonDataKinds.Nickname
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
@@ -12,11 +14,16 @@ class AuthViewModel : ViewModel() {
     private val _emptyInput = MutableLiveData<Boolean>()
             val emptyInput = _emptyInput
 
+    private val _defaultImage = MutableLiveData<Boolean>()
+    val defaultImage = _defaultImage
+
     private val _errorMassage = MutableLiveData<String>()
             val errorMassage = _errorMassage
 
     private val email= mutableStateOf("")
     private val password = mutableStateOf("")
+    private val nickname = mutableStateOf("")
+    private val uri = mutableStateOf<Uri?>(null)
 
     private val authRepository = AuthRepository()
 
@@ -31,13 +38,29 @@ class AuthViewModel : ViewModel() {
                 else{
                     _errorMassage.postValue(result.toString().substringAfter(":"))
                     _emptyInput.postValue(true)
-
-
                 }
             }
         }
         else{
             _errorMassage.value = "fill in all fields"
+            emptyInput.value = true
+        }
+    }
+    fun setDataRegister(){
+        if(email.value != "" && password.value != "" && nickname.value != "" && defaultImage.value == false){
+            viewModelScope.launch {
+                val result = authRepository.registerAccount(email.value, password.value, nickname.value, uri.value!!)
+                if(result.isSuccess){
+                    NavigationManager.navigateTo(NavigationScreen.Chats)
+                }
+                else{
+                    _errorMassage.postValue(result.toString().substringAfter(":"))
+                    _emptyInput.postValue(true)
+                }
+            }
+        }
+        else{
+            _errorMassage.value = "fill in all fields or choose image"
             emptyInput.value = true
         }
     }
@@ -49,6 +72,21 @@ class AuthViewModel : ViewModel() {
     }
     fun setPassword(password : String){
         this.password.value = password
+        if(emptyInput.value == true){
+            emptyInput.value = false
+        }
+    }
+    fun setNickname(nickname : String){
+        this.nickname.value = nickname
+        if(emptyInput.value == true){
+            emptyInput.value = false
+        }
+    }
+    fun setNonDefaultImage(){
+        _defaultImage.value = false
+    }
+    fun setUri(uri:Uri){
+        this.uri.value = uri
         if(emptyInput.value == true){
             emptyInput.value = false
         }
