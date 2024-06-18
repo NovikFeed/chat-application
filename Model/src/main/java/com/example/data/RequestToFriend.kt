07b82b66855c610644772061199406173a6d72c9
ivanovich <vania200345@gmail.com>
2024-06-18 18:42:47 +0300

    Add status parameter for Request class and change way get request list

--- Model/src/main/java/com/example/data/RequestToFriend.kt
+++ Model/src/main/java/com/example/data/RequestToFriend.kt
@@ -3,7 +3,8 @@ package com.example.data
 data class RequestToFriend(
      val uid: String = "",
      val nickname: String = "",
-     val photoUrl: String = ""
+     val photoUrl: String = "",
+     val status : Boolean = true
 ) {
 
 }
